c9f974443d908f0037b481055755a2caff07f442
ktsu2i <kaito.tsu2i@gmail.com>
2026-02-25 00:44:56 +0900

    add talks

--- src/data/talks.ts
+++ src/data/talks.ts
@@ -2,22 +2,21 @@ import type { Talk } from "../lib/types";
 
 export const talks: Talk[] = [
   {
-    id: "talk-1",
-    title: "サンプルトーク: モダンフロントエンド開発",
-    event: "Tech Conference 2025",
-    date: "2025-06-15",
-    speakerdeckUrl: "https://speakerdeck.com/example/modern-frontend",
-    embedUrl: "https://speakerdeck.com/player/example-embed-id",
-    description:
-      "モダンフロントエンド開発のベストプラクティスについて発表しました。",
+    id: "cloudsign-go-conference-mini-2026-sendai",
+    title: "Go パッケージのサプライチェーン攻撃を防ぐ CI を作ってみた",
+    event: "Go Conference mini in Sendai 2026",
+    date: "2026-02-21",
+    speakerdeckUrl: "https://speakerdeck.com/bengo4com/20260221-cloudsign-go-concerence-mini-2026-in-sendai",
+    embedUrl: "https://speakerdeck.com/player/caad8d737da64cecacbeff26a5f0b343",
+    description: "2026年2月21日(土)「Go Concerence mini 2026 in Sendai」における、クラウドサインのエンジニア筒井の登壇資料です。",
   },
   {
-    id: "talk-2",
-    title: "サンプルトーク: Go で作る CLI ツール",
-    event: "Go Meetup Tokyo",
-    date: "2025-03-20",
-    speakerdeckUrl: "https://speakerdeck.com/example/go-cli-tools",
-    embedUrl: "https://speakerdeck.com/player/example-embed-id-2",
-    description: "Go 言語を使った CLI ツールの設計と実装について紹介しました。",
+    id: "cloudsign-coefl-go-benkyokai-2-tsutsui",
+    title: "Understanding Go GC",
+    event: "COEFL Go-JP #2",
+    date: "2025-08-22",
+    speakerdeckUrl: "https://speakerdeck.com/bengo4com/20250822-cloudsign-coefl-go-jp2-tsutsui",
+    embedUrl: "https://speakerdeck.com/player/41bd3405621a4598963d5753725494d0",
+    description: "2025年8月22日(金)開催、「COEFL Go-JP #2」で登壇した、弁護士ドットコム株式会社 クラウドサイン エンジニア の筒井 海人 が登壇した際の資料です。",
   },
 ];
